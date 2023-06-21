fab17e3c42c142a1daa56def54b5f7d809fc32b7
Seth Borrowman <87055552+seth-borrowman@users.noreply.github.com>
2023-06-21 15:05:36 -0500

    DE for 714051

--- DESeq2Analysis.R
+++ DESeq2Analysis.R
@@ -1,5 +1,7 @@
 library(tximeta)
 library(DESeq2)
+library(magrittr)
+library(apeglm)
 
 # Set wd
 setwd("Z:/UDP- Research/RNAseq data")
@@ -14,22 +16,21 @@ for (i in seq_along(1:length(coldata[,1]))) {
                                "quant.sf")
 }
 
-# controls <- as.data.frame(matrix(nrow = 4, ncol = 2))
-# colnames(controls)  <- c("names", "files")
-# controls [,1] <- c("ENCFF446MZM", "ENCFF545DTG", "ENCFF676GUE", "ENCFF730OTJ")
-# for ( i in seq_along(1:length(controls[,1]))) {
-#     controls[i, 2] <- file.path("Controls",
-#                                 paste(controls[i, 1], ".tsv", sep = ""))
-# }
-# coldata <- rbind(coldata, controls)
-# coldata$treat <- c(rep("case", 6), rep("control", 4))
-# coldata$treat <- factor(coldata$treat)
-#cts <- coldata[,1]
-
-se <- tximeta(coldata)
-ddsTxi <- DESeqDataSet(se, design = ~ names)
+### 714051 as outgroup
+coldata51 <- coldata
+coldata51$treat <- c("case", rep("control", 5))
+coldata51$treat %>%
+    factor() %>%
+    relevel(ref = "control")
+se <- tximeta(coldata51)
+ddsTxi <- DESeqDataSet(se, design = ~ treat)
 
 # DE analysis
 keep <- rowSums(counts(ddsTxi)) >= 10 # Filter low count genes
 ddsTxi <- ddsTxi[keep, ]
 dds <- DESeq(ddsTxi)
+resLFC <- lfcShrink(dds, coef = "treat_control_vs_case", type = "apeglm")
+
+plotMA(resLFC)
+
+
