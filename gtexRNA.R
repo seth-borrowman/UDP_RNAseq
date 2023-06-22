38c9574370fec7ff03c640e0d8121f584c62d2c7
Seth Borrowman <87055552+seth-borrowman@users.noreply.github.com>
2023-06-22 14:40:50 -0500

    uncomment things

--- gtexRNA.R
+++ gtexRNA.R
@@ -55,20 +55,22 @@ sig_714051 <- gtex_means %>% filter(abs(udp714051_delta) > 1500)
 
 print("Filtered reads")
 
-scatter_714051 <- ggplot(data = gtex_means, aes(x=mean_tpm, y=udp714051, label = Description)) + 
-    geom_point(size=0.6, color="darkgray", alpha=0.8) + 
+save(file = "gtexRNA.RData")
+
+scatter_714051 <- ggplot(data = gtex_means, aes(x=mean_tpm, y=udp714051, label = Description)) +
+    geom_point(size=0.6, color="darkgray", alpha=0.8) +
     geom_point(data = sig_714051, aes(x=mean_tpm, y=udp714051), size=0.8, color="red", alpha=.9 ) +
-    geom_smooth(method = "lm") + 
+    geom_smooth(method = "lm") +
     scale_x_continuous(trans='log2') + scale_y_continuous(trans='log2') +
-    geom_text_repel(data = sig_714051, 
-                    max.overlaps = 30, 
-                    size = 2.5, 
-                    segment.size=0.1, 
-                    nudge_x=0.06, 
+    geom_text_repel(data = sig_714051,
+                    max.overlaps = 30,
+                    size = 2.5,
+                    segment.size=0.1,
+                    nudge_x=0.06,
                     nudge_y=0.06,
                     max.iter= 200,
-                    point.padding = 0.15, 
-                    segment.alpha = 1, 
+                    point.padding = 0.15,
+                    segment.alpha = 1,
                     box.padding=.15,
                     min.segment.length = unit(0.15, 'lines'))
 
@@ -76,12 +78,12 @@ png(file = "scatterplot.png", width = 1080, height = 1080)
 scatter_714051
 dev.off()
 
-barplot_714051 <- ggbarplot(sig_714051, x='Description', y='udp714051_delta', 
-                            sort.val='asc', 
-                            sort.by.groups = FALSE, 
+barplot_714051 <- ggbarplot(sig_714051, x='Description', y='udp714051_delta',
+                            sort.val='asc',
+                            sort.by.groups = FALSE,
                             x.text.angle=90,
                             ylab = "Patient 714051 Delta From GTEX Mean (TPM)",
-                            xlab = "Gene ID", 
+                            xlab = "Gene ID",
                             lab.size = 1,
                             rotate = TRUE,
                             ggtheme = theme_minimal())
