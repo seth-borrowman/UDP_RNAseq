8d61ae4f1511c3026906b335892e96faa8275c74
Seth Borrowman <87055552+seth-borrowman@users.noreply.github.com>
2023-08-18 13:46:15 -0500

    Loop through all participants

--- DESeq_GTEx.R
+++ DESeq_GTEx.R
@@ -4,35 +4,11 @@ library(tidyverse)
 library(ashr)
 library(biomaRt)
 library(ggrepel)
+library(tximport)
 
 # Set wd
 setwd("Z:/UDP- Research/RNAseq data")
-
-
-# Load data
-coldata <- as.data.frame(matrix(nrow = 6, ncol = 2))
-colnames(coldata) <- c("names", "files")
-coldata[,1] <- c("714051", "715986", "715991", "731949", "731952", "731956")
-for (i in seq_along(1:length(coldata[,1]))) {
-    coldata[i, 2] <- file.path("quants",
-                               paste(coldata[i, 1], "_quant", sep = ""),
-                               "quant.sf")
-}
-
-case <- read_delim("quants/UIRDB20230008/quant.sf")
-mart <- useEnsembl(biomart = "ensembl", dataset = "hsapiens_gene_ensembl")
-geneNames <- getBM(filters = "ensembl_transcript_id_version",
-                   attributes = c("ensembl_gene_id_version",
-                                  "ensembl_transcript_id_version",
-                                  "hgnc_symbol"),
-                   values = case$Name,
-                   mart = mart)
-case <- merge(case, geneNames,
-              by.x = "Name", by.y = "ensembl_transcript_id_version")
-
-case_sum <- case %>%
-    group_by(ensembl_gene_id_version) %>%
-    summarise(Reads = sum(NumReads))
+ifelse(!dir.exists('DE'), dir.create('DE'), FALSE)
 
 # Import GTEx controls
 gtex <- read_delim("Readsgtex.gct",
@@ -40,46 +16,87 @@ gtex <- read_delim("Readsgtex.gct",
     rename(gene_name = Name) %>%
     dplyr::select(-c(id, Description))
 
-# Merge with case
-gtex_case <- merge(gtex,
-                   case_sum %>% dplyr::select(Reads, ensembl_gene_id_version),
-                   by.x = "gene_name", by.y = "ensembl_gene_id_version")
-row.names(gtex_case) <- gtex_case[,1]
-gtex_case <- gtex_case[,-1]
+mart <- useEnsembl(biomart = "ensembl", dataset = "hsapiens_gene_ensembl")
 
 
+# Load data
+participants <- list.files("quants/")
 
-condition <- rep("Control", ncol(gtex_case))
-condition[which(colnames(gtex_case) == "Reads")] <- "Case"
-ddsTxi <- DESeqDataSetFromMatrix(round(gtex_case), DataFrame(condition),
+for (i in participants) {
+    
+    # Load case data
+    case <- tximport(paste("quants/", i, "/quant.sf", sep = ""),
+                     type = "salmon",
+                     txOut = T) %>%
+        as.data.frame() %>%
+        rownames_to_column() %>%
+        rename(ensembl_transcript_id_version = rowname, NumReads = counts)
+    
+    geneNames <- getBM(filters = "ensembl_transcript_id_version",
+                       attributes = c("ensembl_transcript_id_version",
+                                      "ensembl_gene_id_version",
+                                      "hgnc_symbol"),
+                       values = case$ensembl_transcript_id_version,
+                       mart = mart)
+    case <- merge(case, geneNames)
+    
+    # IIHG gives results by transcript ID, GTEx is by gene - convert to gene
+    case_sum <- case %>%
+        group_by(ensembl_gene_id_version) %>%
+        summarise(Reads = sum(NumReads))
+
+
+    # Merge with controls
+    gtex_case <- merge(gtex, case_sum,
+                       by.x = "gene_name", by.y = "ensembl_gene_id_version")
+    row.names(gtex_case) <- gtex_case[,1] # Make genes column name
+    gtex_case <- gtex_case[,-1]
+
+    # Set case/control condition
+    condition <- rep("Control", ncol(gtex_case))
+    condition[which(colnames(gtex_case) == "Reads")] <- "Case"
+
+    # Create DSEq dataset
+    ddsTxi <- DESeqDataSetFromMatrix(round(gtex_case), DataFrame(condition),
                                  ~ condition, tidy = F)
 
-dds <- DESeq(ddsTxi)
-resLFC <- lfcShrink(dds, coef = "condition_Control_vs_Case", type = "ashr")
-results <- results(dds)
-resultsDF <- as.data.frame(resLFC)
-resultsDF$GeneID <- resLFC@rownames
+    # DESeq analysis with ASHR shrinkage
+    dds <- DESeq(ddsTxi)
+    resLFC <- lfcShrink(dds, coef = "condition_Control_vs_Case", type = "ashr")
+    resultsDF <- as.data.frame(resLFC)
+    resultsDF$GeneID <- resLFC@rownames
 
-
-resultsDF <- left_join(resultsDF,
+    # Move results to df with gene symbol
+    resultsDF <- left_join(resultsDF,
                        geneNames %>%
-                           dplyr::select(c("ensembl_gene_id_version", "hgnc_symbol")) %>%
+                           dplyr::select(c("ensembl_gene_id_version",
+                                           "hgnc_symbol")) %>%
                            unique(),
                    by = c("GeneID" = "ensembl_gene_id_version"))
 
-resultsDF <- resultsDF %>%
-    mutate(sig = case_when(
-        -log(pvalue) >= -log(0.05/nrow(resultsDF)) & abs(log2FoldChange) >= 2 ~ 1,
-        .default = 0
-    ))
-resultsDF$sig <- factor(resultsDF$sig, ordered = is.ordered(c(0, 1)))
-ggplot(data = resultsDF, aes(x = log2FoldChange, y = -log(pvalue), color = sig)) +
-    geom_point(show.legend = F) +
-    geom_vline(xintercept = c(-2, 2), color = "pink") +
-    geom_hline(yintercept = -log(0.05/nrow(resultsDF)), color = "lightblue") +
-    scale_color_manual(values = c("grey", "red")) +
-    theme_minimal() +
-    geom_label_repel(data = resultsDF %>%
-                        filter(sig == 1),
-        aes(label = hgnc_symbol)) +
-    theme(legend.position = "none")
+    # Find significant results - Bonferroni p, arbitrary log2FC >= 2
+    resultsDF <- resultsDF %>%
+        mutate(sig = case_when(
+            -log(pvalue) >= -log(0.05/nrow(resultsDF)) &
+                abs(log2FoldChange) >= 2 ~ 1,
+            .default = 0
+        ))
+    resultsDF$sig <- factor(resultsDF$sig, ordered = is.ordered(c(0, 1)))
+    write_csv(paste("DE/", i, ".csv", sep = ""))
+
+    # Plot
+    png(filename = paste("DE/", i, "_volcano.png", sep = ""), ,
+        width = 1400, height = 800)
+    ggplot(data = resultsDF, aes(x = log2FoldChange, y = -log(pvalue), color = sig)) +
+        geom_point(show.legend = F) +
+        #geom_vline(xintercept = c(-2, 2), color = "pink") +
+        geom_hline(yintercept = -log(0.05/nrow(resultsDF)), color = "lightblue") +
+        scale_color_manual(values = c("grey", "red")) +
+        theme_minimal() +
+        geom_label_repel(data = resultsDF %>%
+                            filter(sig == 1),
+            aes(label = hgnc_symbol)) +
+        theme(legend.position = "none") +
+        ggtitle(i)
+    dev.off()
+}
